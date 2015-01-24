b4f7cfe3216d290a4b243d994733f78a20733025
John Bentley <jwbentle@ucsc.edu>
2015-01-24 14:06:58 -0800

    Added shooting the player and exit back in.

--- js/hitExit.js
+++ js/hitExit.js
@@ -1,4 +1,6 @@
-function hitExit (player1, player2, exit) {
+function hitExit (exit) {j
+	exit.kill();
+
     //  Add and update the score
     score += 10;
     scoreText.text = 'Score: ' + score;
---
+++ js/shootPlayer.js
@@ -0,0 +1,4 @@
+function shootPlayer(player1, player2, p1bullets, p2bullets) {
+	//p1bullets.kill();
+	//p2bullets.kill();
+}
